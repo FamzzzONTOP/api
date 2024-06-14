e1605c272e6eb0e2b3565933401ebfdcf479d3cd
FamzzzONTOP <168281239+FamzzzONTOP@users.noreply.github.com>
2024-06-14 20:16:32 +0700

    main.py

--- main.py
+++ main.py
@@ -3,37 +3,20 @@ import requests
 
 app = Flask(__name__)
 
-# API endpoint and key
-API_URL = 'https://madkung.vercel.app/fluxus-api'
-API_KEY = 'XqzyaenZishd33axPYPz'
-
-@app.route('/fluxus', methods=['GET'])
-def fluxus():
-    # Ambil parameter URL dari permintaan
+@app.route('/api/bypass', methods=['GET'])
+def bypass():
     target_url = request.args.get('url')
-
-    # Pastikan parameter URL ada
     if not target_url:
-        return jsonify({'error': 'URL parameter is required'}), 400
+        return jsonify({"error": "URL parameter is missing"}), 400
 
-    # Buat permintaan ke API eksternal
-    try:
-        response = requests.get(API_URL, params={'url': target_url, 'api_key': API_KEY})
-    except Exception as e:
-        return jsonify({'error': f'Failed to fetch data from external API: {str(e)}'}), 500
+    api_url = "https://2c8f98d8-9741-4ae2-b2c4-3e9d12cc68e1-00-1huumoqlhh1yg.sisko.replit.dev/bypass"
+    params = {'url': target_url}
+    response = requests.get(api_url, params=params)
 
-    # Periksa apakah permintaan berhasil
-    if response.status_code != 200:
-        return jsonify({'error': 'Failed to fetch data from external API'}), response.status_code
-
-    # Ambil data JSON dari respons
-    data = response.json()
-
-    # Ganti teks 'ethos' dengan 'api' dalam data respons
-    modified_data = str(data).replace('mad6453', 'famzz')
-
-    # Kembalikan data yang dimodifikasi sebagai JSON
-    return jsonify(eval(modified_data))
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch data"}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
